f442f07ca07c068b4f868fbb60481cb9764c84b9
Jesse Santos <jessesantos02001.JS@gmail.com>
2022-06-24 20:06:20 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-
 import "./App.css";
-import Carousel from "./Carousel";
-import data from "./data";
+// import Carousel from "./Carousel";
+// import data from "./data";
+import Navbar from "./Navbar";
+import Shop from "./Pages/Shop";
+import About from "./Pages/About"
+import Home from "./Pages/Home"
 
 function App() {
+  console.log(window.location)
+   let component
+   switch (window.location.pathname){
+    case "/":
+       component = <Home />
+      break
+      case "/shop":
+         component = <Shop />
+        break
+        case "/about":
+          component = <About />
+          break
+  }
   return (
-    <div className="App">
-      <Carousel images={data} />
+    <div>
+    <>
+      <Navbar/>
+      <div className="container">
+      {component}
+      </div>
+    </>
     </div>
+    // <div className="App">
+
+    //   <Carousel images={data} />
+    // </div>
   );
 }
 
